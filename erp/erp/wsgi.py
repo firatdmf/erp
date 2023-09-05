70c6c3a5b9ee449593935c981c9cbf2c1d2c683c
Muhammed Firat Ozturk <firatdmf@gmail.com>
2023-09-05 10:43:06 +0300

    changed the wsgi.py back to normal

--- erp/erp/wsgi.py
+++ erp/erp/wsgi.py
@@ -13,7 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp.settings')
 
-# application = get_wsgi_application()
-
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()

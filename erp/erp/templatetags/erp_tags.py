313df5846640fa805fc9e261d97e53fc95d62a69
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-12-15 14:45:43 +0300

    added the country of the day to market

--- erp/erp/templatetags/erp_tags.py
+++ erp/erp/templatetags/erp_tags.py
@@ -3,11 +3,29 @@ from django.template.loader import render_to_string
 from crm.models import Contact, Company
 from django.utils import timezone
 import datetime
+import calendar
 register = template.Library()
+
+
+country_index = 0
 @register.simple_tag
 def dashboard_component(csrf_token):
     today = timezone.localtime(timezone.now()).date()
-    # print(today)
+    # today_day = (str(today)).split('-')[2]
+    # below is the current week number of the current year
+    week_number = datetime.date.today().isocalendar()[1] 
+    country_of_the_day = '' #initializing the variable
+    crm_countries = ['Spain','Italy','Portugal','Poland','Germany','France','Japan','Romania','Hungary','USA','Canada','UK']
+    # We need to make sure today is not Sunday, because we do not work on Sundays (sunday is the 6th day of the week iterating from zero)
+    if (today.weekday()!=6):
+        # If the week number is not even (is odd) then select the first 6 countries from the list and pick one according to the day of the week, where 0 is monday
+        if week_number % 2 != 0:
+            country_of_the_day = crm_countries[:6][today.weekday()]
+        else:
+            country_of_the_day = crm_countries[-6:][today.weekday()]
+
+
+
     # contact = Contact.objects.filter(name__icontains="firat")[0]
     contacts = Contact.objects.filter(created_at__gte=today)
     companies = Company.objects.filter(created_at__gte=today)
@@ -15,6 +33,7 @@ def dashboard_component(csrf_token):
     return render_to_string('components/dashboard.html',{
         'csrf_token':csrf_token,
         'number_of_leads_added':number_of_leads_added,
+        'country_of_the_day':country_of_the_day,
     })
 
 @register.simple_tag
